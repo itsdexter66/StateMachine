8ebb3d64ad9e636a395f5ef546d8101ab3e59470
Dexter <itsdexter66@gmail.com>
2019-05-10 13:34:47 +0200

    EnemyAttack

---
+++ StateMachine/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject enemy;
+    float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Attacking();
+    }
+
+    void Attacking()
+    {
+       distance = Vector2.Distance(player.transform.position, enemy.transform.position);
+        if (distance <= 1.5)
+        {
+            Debug.Log("Attacked");
+        }
+    }
+}
